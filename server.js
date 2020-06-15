0f69bbb2ee24a5eee08fe357c6b8e5b9c4b21a31
sahanpasindu <sahanpasindu07@gmail.com>
2020-06-15 10:58:41 +0530

    create new user

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful!'));
 
