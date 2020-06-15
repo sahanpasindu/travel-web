dd7d0efc27b3185b1b0ca29fd60e341adfe0fa4e
sahanpasindu <sahanpasindu07@gmail.com>
2020-06-15 10:25:06 +0530

    complete userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,36 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: [true, 'Please tell us your name'],
+         trim: true
+      },
+      email: {
+         type: String,
+         required: [true, 'Please provide your email'],
+         unique: true,
+         lowercase: true,
+         trim: true,
+         validate: [validator.isEmail, 'Please provide a valid email']
+      },
+      photo: {
+         type: String,
+      },
+      password: {
+         type: String,
+         required: [true, 'Please provide your passwrod'],
+         minlength: 8
+      },
+      passwordConfirm: {
+         type: String,
+         required: [true, 'Please confirm your password']
+      }
+
+   }
+);
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
